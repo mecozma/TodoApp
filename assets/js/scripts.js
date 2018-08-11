fb58d14c40ecbd812c9c3385eb40fea466b728f4
mecozma <mecozma@gmail.com>
2018-08-12 00:21:46 +0100

    Added jQuery functionality to delete the todo when the X is clicked

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,8 +1,16 @@
-// check off specific todos by clicking
 $(document).ready(function() {
+    // check off specific todos by clicking
     $('li').on('click', function() {
     
         $(this).toggleClass('completed');
 
      });
+
+    //  Click on X to delete Todos
+    $('span').on('click', function(e) {
+        e.stopPropagation();
+        $(this).parent().fadeOut(500, function() {
+            $(this).remove();
+        });
+    })
 });
