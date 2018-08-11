3df3d40e24496135f17b920fb7c24830f4662253
mecozma <mecozma@gmail.com>
2018-08-11 23:55:28 +0100

    Added the html base and the jQuery logic to mark the completed todos

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,1 +1,20 @@
-alert('connected')
+// check off specific todos by clicking
+$(document).ready(function() {
+    $('li').on('click', function() {
+        // if li is gray
+        if($(this).css('color') === 'rgb(128, 128, 128)') {
+            // turn it black
+            $(this).css({
+                textDecoration: 'none',
+                color: 'black'
+            });
+        } else {
+            // turn it gray
+            $(this).css({
+                textDecoration: 'line-through',
+                color: 'gray'
+               });
+        }
+     
+    });
+});
