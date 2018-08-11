4d1762be3d3a3b585e0350ae687d0b08a2d2abc5
mecozma <mecozma@gmail.com>
2018-08-12 00:00:27 +0100

    Modified the jQuery code to  mark/unmark the completed todos by toggling a css  class

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,20 +1,8 @@
 // check off specific todos by clicking
 $(document).ready(function() {
     $('li').on('click', function() {
-        // if li is gray
-        if($(this).css('color') === 'rgb(128, 128, 128)') {
-            // turn it black
-            $(this).css({
-                textDecoration: 'none',
-                color: 'black'
-            });
-        } else {
-            // turn it gray
-            $(this).css({
-                textDecoration: 'line-through',
-                color: 'gray'
-               });
-        }
-     
-    });
+    
+        $(this).toggleClass('completed');
+
+     });
 });
