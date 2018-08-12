c53e5eb62e8ce8a66c9d67b223251b6ae4e184f3
mecozma <mecozma@gmail.com>
2018-08-12 03:00:20 +0100

    New todo can be added and also modified the jQuery code delete/cross the new added todos

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,16 +1,24 @@
 $(document).ready(function() {
     // check off specific todos by clicking
-    $('li').on('click', function() {
-    
+    $('ul').on('click', 'li', function() {
         $(this).toggleClass('completed');
-
      });
 
     //  Click on X to delete Todos
-    $('span').on('click', function(e) {
+    $('ul').on('click', 'span', function(e) {
         e.stopPropagation();
         $(this).parent().fadeOut(500, function() {
             $(this).remove();
         });
-    })
+    });
+
+    // Add a new todo
+    $("input[type='text']").on('keypress', function(event) {
+        if (event.which === 13) {
+            // collecting the new todo from the input
+            var newTodoValue = $('input').val();
+            $(this).val("");
+            $('ul').append('<li><span>X</span>' + newTodoValue + '</li>')
+        }
+    });
 });
